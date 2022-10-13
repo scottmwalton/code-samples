dbf934416a7b3e1ba05ca54469f47d11ff4a6eaf
Scott Walton <scottmwalton@hotmail.com>
2022-10-13 11:13:10 -0500

    added diagrams with functionality

---
+++ SyncfusionDiagramSample/SyncfusionDiagramSample/MockDataService.cs
@@ -0,0 +1,42 @@
+﻿namespace SyncfusionDiagramSample
+{
+	public class MockDataService
+	{
+		protected readonly List<DisplayNode> myRefreshData;
+		protected readonly List<DisplayNode> myDoLayoutData;
+
+		public MockDataService()
+		{
+			myRefreshData = new List<DisplayNode>() 
+			{
+				new DisplayNode { Id = 1, NodeName = "Top Node", NodeType = "Main Item", ParentId = null },
+				new DisplayNode { Id = 2, NodeName = "Child Node", NodeType = "First Sub Item", ParentId = 1 },
+				new DisplayNode { Id = 3, NodeName = "Child Node", NodeType = "Second Sub Item", ParentId = 1 },
+				new DisplayNode { Id = 4, NodeName = "Second Level Node", NodeType = "Child Item", ParentId = 3 },
+				new DisplayNode { Id = 5, NodeName = "Third Level Node", NodeType = "Child Item", ParentId = 4 },
+			};
+			myDoLayoutData = new List<DisplayNode>();
+			myDoLayoutData.AddRange(myRefreshData.ToList());
+		}
+
+		public List<DisplayNode> GetRefreshData()
+		{
+			return myRefreshData.ToList();
+		}
+
+		public void AddRefreshNode(DisplayNode displayNode)
+		{
+			myRefreshData.Add(displayNode);
+		}
+
+		public List<DisplayNode> GetDoLayoutData()
+		{
+			return myDoLayoutData.ToList();
+		}
+
+		public void AddDoLayoutNode(DisplayNode displayNode)
+		{
+			myDoLayoutData.Add(displayNode);
+		}
+	}
+}
