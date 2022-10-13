ffcbf72e3d3c4904885904d0fbc7b23742769fff
Scott Walton <scottmwalton@hotmail.com>
2022-10-13 11:14:55 -0500

    added diagrams with functionality (#7)

---
+++ SyncfusionDiagramSample/SyncfusionDiagramSample/DisplayNode.cs
@@ -0,0 +1,11 @@
+﻿namespace SyncfusionDiagramSample
+{
+
+	public class DisplayNode
+	{
+		public int Id { get; set; }
+		public string NodeName { get; set; } = string.Empty;
+		public string NodeType { get; set; } = string.Empty;
+		public int? ParentId { get; set; }
+	}
+}
